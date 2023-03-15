238d1c48aa06fdf684454cfa0caae6450a0b197a
Webs95 <alexandersemashko1995@yandex.ru>
2023-03-15 10:15:02 +0300

    feat: add top-page model

---
+++ src/top-page/top-page.model.ts.ts
@@ -0,0 +1,13 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export class TopPageModelTs {
+  firstLevelCategory: TopLevelCategory;
+  secondCategody: string;
+  title: string;
+  category: string;
+}
