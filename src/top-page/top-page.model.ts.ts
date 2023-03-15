2041b96e5c6f4c6f16f48d02a011651fcd138440
Webs95 <alexandersemashko1995@yandex.ru>
2023-03-15 10:18:19 +0300

    feat: add model

--- src/top-page/top-page.model.ts.ts
+++ src/top-page/top-page.model.ts.ts
@@ -10,4 +10,17 @@ export class TopPageModelTs {
   secondCategody: string;
   title: string;
   category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: {
+    title: string;
+    description: string;
+  }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
 }
